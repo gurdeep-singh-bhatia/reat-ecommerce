b2dcc9030a5e559ef0fcc5a91b8396ec65582ebd
gurdeep-singh-bhatia <gsbhatia111@gmail.com>
2023-03-15 19:05:49 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
+import {GlobalStyle} from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+
+
+const App = () => {
+
+
+  const theme = {
+    colors : {
+      bg : "#000"
+    },
+  };
+
+  return (
+
+    <ThemeProvider theme={theme}>
+    <BrowserRouter> 
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart/" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+    </ThemeProvider>
+  );
+};
+
+export default App;
